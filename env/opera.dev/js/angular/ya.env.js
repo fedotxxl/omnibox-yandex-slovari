f88035e1a72d98815b4a81f1e0073a411ddca23f
fedor.belov <fedor.belov@mail.ru>
2014-12-30 01:56:29 +0300

    opera

--- env/opera.dev/js/angular/ya.env.js
+++ env/opera.dev/js/angular/ya.env.js
@@ -5,8 +5,8 @@ angular.module('common').
     constant('_browser', 'opera').
     factory('_privateRouter', function() {
         return {
-            rateItUrl: "",
-            shareUrl: "",
+            rateItUrl: "https://addons.opera.com/extensions/details/omnibox-yandex-dictionary/",
+            shareUrl: "http://bit.ly/1vDe8g1",
             hashMemPluginUrl: "https://addons.opera.com/extensions/details/hashmemcom-zakladki-zametki-v-omnibox/",
             googleTranslatePluginUrl: "https://addons.opera.com/extensions/details/omnibox-google-translate/"
         }
